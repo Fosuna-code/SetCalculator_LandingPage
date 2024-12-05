619f4f95c3b913e5823c34fa5c347cc099ab38ac
Fernando Osuna Manzo <ferosuna2001@gmail.com>
2024-12-05 01:42:07 -0600

    v1

--- skeleton.js
+++ skeleton.js
@@ -1,22 +1,22 @@
-const handleImageLoad = () => {
+// const handleImageLoad = () => {
 	
-	container.classList.remove('skeleton');
-	image.removeEventListener('load', handleImageLoad)
-}
+// 	container.classList.remove('skeleton');
+// 	image.removeEventListener('load', handleImageLoad)
+// }
 
-const container = document.querySelector(".cards_section");
-console.log(container);
+// const container = document.querySelector(".cards_section");
+// console.log(container);
 
-const cardTemplate = document.getElementById("card_template");
-console.log(cardTemplate);
+// const cardTemplate = document.getElementById("card_template");
+// console.log(cardTemplate);
 
-for (let i = 0; i < 6; i++) {
-	console.log(cardTemplate.content);
+// for (let i = 0; i < 6; i++) {
+// 	console.log(cardTemplate.content);
 	
-	container.append(cardTemplate.content.cloneNode(true));
-}
-const image = document.getElementById('card_img');
-if ( !image.complete) {
-	image.addEventListener('load', handleImageLoad);
-	image.addEventListener('error', handleImageLoad);
-}
+// 	container.append(cardTemplate.content.cloneNode(true));
+// }
+// const image = document.getElementById('card_img');
+// if ( !image.complete) {
+// 	image.addEventListener('load', handleImageLoad);
+// 	image.addEventListener('error', handleImageLoad);
+// }
